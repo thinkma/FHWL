8f802d6f97de12f8537a0fabc2ce098a4ecd350f
weima <weima@weimadeMacBook-Pro.local>
2018-05-31 22:35:33 +0800

    第一次提交 建立项目

---
+++ sourceThree/sourceThree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sourceThree
+//
+//  Created by weima on 2018/5/31.
+//  Copyright © 2018年 weima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
